95893e6cf37e66e2ff6de9fb9537a87fedc5cd71
Aarav Kumar <your-email@example.com>
2026-01-10 20:36:11 +1100

    Add secret bypass button to maintenance page with password access

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import Maintenance from './maintenance'
+import { useEffect, useState } from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
@@ -7,7 +8,14 @@ import Footer from '../components/Footer'
 import { CartProvider } from '../context/CartContext'
 import { CurrencyProvider } from '../context/CurrencyContext'
 
-export default function App({ Component, pageProps }: AppProps) {
+  const [bypass, setBypass] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setBypass(localStorage.getItem('mineclash_bypass') === 'true');
+    }
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Head>
@@ -18,7 +26,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <CartProvider>
           <Header />
           <main className="flex-1 container mx-auto px-4 py-8">
-            <Maintenance />
+            {bypass ? <Component {...pageProps} /> : <Maintenance />}
           </main>
           <Footer />
         </CartProvider>
